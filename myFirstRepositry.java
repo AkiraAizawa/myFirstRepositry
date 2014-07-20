906293a7dd115a2d51e55556c9aa1fe052d6424d
Akira Aizawa <akira.git@gmail.com>
2014-07-21 00:17:02 +0900

    Add another new line.

--- myFirstRepositry.java
+++ myFirstRepositry.java
@@ -2,5 +2,6 @@ class myFirstRepositry {
 	public static void main(String[] args) {
 		System.out.println("This is my first repositry!");
 		System.out.println("Add branch-A!");
+		System.out.println("Add 2nd line.");
 	}
 }

9f4674d8a6c0932553d6cb249e919f79e7e965d5
Akira Aizawa <akira.git@gmail.com>
2014-07-21 17:45:21 +0900

    Add new function.

--- myFirstRepositry.java
+++ myFirstRepositry.java
@@ -3,5 +3,10 @@ class myFirstRepositry {
 		System.out.println("This is my first repositry!");
 		System.out.println("Add branch-A!");
 		System.out.println("Add 2nd line.");
+		log("Add new function.");
+	}
+
+	private static void log(String message) {
+		System.out.println(message);
 	}
 }
